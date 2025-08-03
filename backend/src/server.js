504c332a7e47ef92507708804e27f50566b8994d
Siddharth <siddharth20630901@gmail.com>
2025-08-03 17:11:36 +0545

    initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,27 @@
 import express from "express";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 import cors from "cors";
-import cookieParser from "cookie-parser"
-import authRoutes from "./routes/auth.route.js"
-import  userRoutes from "./routes/user.route.js"
-import chatRoutes from "./routes/chat.route.js"
-import { connectDB } from "./lib/db.js";
 import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+
+import { connectDB } from "./lib/db.js";
+
 const app = express();
 const PORT = process.env.PORT;
+
 const __dirname = path.resolve();
 
-app.use(cors(
-    {
-        origin:"http://localhost:5173",
-        credentials:true
-    }
-))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -32,8 +37,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-
-app.listen(PORT,()=>{
-    console.log(`server is running in on port ${PORT}`)
-    connectDB();
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
