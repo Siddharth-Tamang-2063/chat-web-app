e864d62b053cba516d7bc31c63242b40f7ff2435
Siddharth <siddharth20630901@gmail.com>
2026-03-02 12:03:50 +0545

    fix cors for vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,26 @@
 import dns from 'dns';
 dns.setServers(['8.8.8.8', '8.8.4.4']);
-
 import express from "express";
 import "dotenv/config";
-// ... rest of your imports
-
-
-
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
-
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
-
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
-
 const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true, // allow frontend to send cookies
+    origin: [
+      "http://localhost:5173",
+      "https://chat-web-app-5w2c.vercel.app",
+    ],
+    credentials: true,
   })
 );
 
@@ -38,7 +33,6 @@ app.use("/api/chat", chatRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
